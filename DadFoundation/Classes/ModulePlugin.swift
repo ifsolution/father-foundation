5c8a2a0a84ac1b3a9ce503f50e9a5776469aa757
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-05-25 17:07:17 +0700

    Move ModulePlugin to Foundation

---
+++ DadFoundation/Classes/ModulePlugin.swift
@@ -0,0 +1,22 @@
+//
+//  ModulePlugin.swift
+//  DadFoundation
+//
+//  Created by FOLY on 5/22/21.
+//
+
+import Boardy
+import Foundation
+
+public protocol MainComponent: ModuleContainer {
+    /// Main producer
+    var producer: ActivableBoardProducer { get }
+
+    func append(module: ModuleLoader)
+}
+
+public protocol ModulePlugin {
+    var identifier: BoardID { get }
+
+    func apply(for main: MainComponent)
+}
