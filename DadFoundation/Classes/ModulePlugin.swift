7afab5037caf87ccbaf01dc8b3537a1c7e6e0371
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-17 09:10:56 +0700

    Add options to MainComponent

--- DadFoundation/Classes/ModulePlugin.swift
+++ DadFoundation/Classes/ModulePlugin.swift
@@ -8,7 +8,50 @@
 import Boardy
 import Foundation
 
+extension MainOptions.Key {
+    public static let environment = MainOptions.Key(rawValue: "environment")
+}
+
+public struct MainOptions {
+    public struct Key: RawRepresentable, Hashable {
+        public typealias RawValue = String
+
+        public let rawValue: RawValue
+
+        public init(rawValue: RawValue) {
+            self.rawValue = rawValue
+        }
+
+        public func hash(into hasher: inout Hasher) {
+            hasher.combine(rawValue)
+        }
+    }
+
+    private var values: [Key: Any]
+
+    public init(_ values: [Key: Any] = [:]) {
+        self.values = values
+    }
+
+    public static let `default` = MainOptions()
+
+    public subscript<Value>(key: Key) -> Value? {
+        return self[key] as? Value
+    }
+
+    public subscript(key: Key) -> Any {
+        get {
+            return values[key]
+        }
+
+        set {
+            values[key] = newValue
+        }
+    }
+}
+
 public protocol MainComponent {
+    var options: MainOptions { get }
     var producer: BoardDynamicProducer { get }
 }
 
